e823d4e47aaaa3ea5e052296aba18675ff46ff0e
balakrishnavasa <balakrishna@spinoinc.com>
2017-10-10 00:28:25 +0530

    buttons changed

--- Tuts/Src/scripts/directives/custom_directives.js
+++ Tuts/Src/scripts/directives/custom_directives.js
@@ -149,87 +149,87 @@ app.factory('ButtonService', function() {
     var buttons=[];
     factory.permissionbuttons=function(str){
         buttons=[{"btn_value":"Submit","active":true}];
-        // if(str==3){
-        //     buttons=[
-        //         {"name":"submit","btn_value":"Submit","active":true}
-        //         // ,
-        //         // {"name":"close","btn_value":"Close","active":true}
-        //     ];
-        // }
-        // else{
-        //     buttons=[
-        //         {"name":"submit","btn_value":"Submit","active":true}
-        //         // ,
-        //         // {"name":"close","btn_value":"Close","active":true}
-        //     ];
-        // }
+        if(str==3){
+            buttons=[
+                {"name":"submit","btn_value":"Submit","active":true}
+                ,
+                {"name":"close","btn_value":"Close","active":true}
+            ];
+        }
+        else{
+            buttons=[
+                {"name":"submit","btn_value":"Submit","active":true}
+                ,
+                {"name":"close","btn_value":"Close","active":true}
+            ];
+        }
         return buttons;
     }
     factory.permissionbuttonsupdate=function(str,status){
         buttons=[{"btn_value":"Submit","status":"U","active":true}];
-        // if(str==3){
-        //     switch (status ) {
-        //         case "N":
-        //         buttons=[                    
-        //             {"name":"update","btn_value":"Update","status":"U","active":true},
-        //             {"name":"delete","btn_value":"Delete","status":"D","active":true},                     
-        //             {"name":"approve","btn_value":"Approve","status":"Y","active":true},                
-        //             {"name":"approval_request","btn_value":"Approval request","status":"P","active":true},
-        //             // {"name":"close","btn_value":"Close","active":true},
-        //             {"name":"add","btn_value":"Add New","active":true}
-        //             ];
-        //             break;
-        //             case "P":
-        //             buttons=[
-        //             {"name":"update","btn_value":"Update","status":"U","active":true},
-        //             {"name":"delete","btn_value":"Delete","status":"D","active":true},
-        //             {"name":"approve","btn_value":"Approve","status":"Y","active":true},                                        
-        //             {"name":"approval_request","btn_value":"Approval request","status":"P","active":true},
-        //             // {"name":"close","btn_value":"Close","active":true},
-        //             {"name":"add","btn_value":"Add New","active":true}
-        //         ];
-        //         break;
-        //         case "Y":
-        //         buttons=[                          
-        //             {"name":"update","btn_value":"Update","status":"U","active":true},
-        //             {"name":"delete","btn_value":"Delete","status":"D","active":true},                  
-        //             {"name":"disapprove","btn_value":"DisApprove","status":"N","active":true},                    
-        //             // {"name":"close","btn_value":"Close","active":true},
-        //             {"name":"add","btn_value":"Add New","active":true}
-        //         ];
-        //         break;}
+        if(str==3){
+            switch (status ) {
+                case "N":
+                buttons=[                    
+                    {"name":"update","btn_value":"Update","status":"U","active":true},
+                    {"name":"delete","btn_value":"Delete","status":"D","active":true},                     
+                    {"name":"approve","btn_value":"Approve","status":"Y","active":true},                
+                    {"name":"approval_request","btn_value":"Approval request","status":"P","active":true},
+                    {"name":"close","btn_value":"Close","active":true},
+                    {"name":"add","btn_value":"Add New","active":true}
+                    ];
+                    break;
+                    case "P":
+                    buttons=[
+                    {"name":"update","btn_value":"Update","status":"U","active":true},
+                    {"name":"delete","btn_value":"Delete","status":"D","active":true},
+                    {"name":"approve","btn_value":"Approve","status":"Y","active":true},                                        
+                    {"name":"approval_request","btn_value":"Approval request","status":"P","active":true},
+                    {"name":"close","btn_value":"Close","active":true},
+                    {"name":"add","btn_value":"Add New","active":true}
+                ];
+                break;
+                case "Y":
+                buttons=[                          
+                    {"name":"update","btn_value":"Update","status":"U","active":true},
+                    {"name":"delete","btn_value":"Delete","status":"D","active":true},                  
+                    {"name":"disapprove","btn_value":"DisApprove","status":"N","active":true},                    
+                    {"name":"close","btn_value":"Close","active":true},
+                    {"name":"add","btn_value":"Add New","active":true}
+                ];
+                break;}
                 
-        //     }
-        //     else{
-        //         switch (status ) {
-        //             case "N":
-        //             buttons=[
-        //                 {"name":"update","btn_value":"Update","status":"U","active":true},
-        //                 {"name":"delete","btn_value":"Delete","status":"D","active":true}, 
-        //                 {"name":"approval_request","btn_value":"Approval request","status":"P","active":true},
-        //                 // {"name":"close","btn_value":"Close","active":true},
-        //                 {"name":"add","btn_value":"Add New","active":true}];
-        //                 break;
-        //                 case "P":
-        //                 buttons=[
-        //                 {"name":"update","btn_value":"Update","status":"U","active":true},
-        //                 {"name":"delete","btn_value":"Delete","status":"D","active":true},
-        //                 {"name":"approve","btn_value":"Approve","status":"Y","active":true},                                        
-        //                 {"name":"approval_request","btn_value":"Approval request","status":"P","active":true},
-        //                 // {"name":"close","btn_value":"Close","active":true},
-        //                 {"name":"add","btn_value":"Add New","active":true}
-        //             ];
-        //             break;
-        //             case "Y":
-        //             buttons=[                                       
-        //                 {"name":"update","btn_value":"Update","status":"U","active":true},
-        //                 {"name":"delete","btn_value":"Delete","status":"D","active":true},                  
-        //                 {"name":"disapprove","btn_value":"DisApprove","status":"N","active":true},                    
-        //                 // {"name":"close","btn_value":"Close","active":true},
-        //                 {"name":"add","btn_value":"Add New","active":true}
-        //             ];
-        //             break;}
-        //         }
+            }
+            else{
+                switch (status ) {
+                    case "N":
+                    buttons=[
+                        {"name":"update","btn_value":"Update","status":"U","active":true},
+                        {"name":"delete","btn_value":"Delete","status":"D","active":true}, 
+                        {"name":"approval_request","btn_value":"Approval request","status":"P","active":true},
+                        {"name":"close","btn_value":"Close","active":true},
+                        {"name":"add","btn_value":"Add New","active":true}];
+                        break;
+                        case "P":
+                        buttons=[
+                        {"name":"update","btn_value":"Update","status":"U","active":true},
+                        {"name":"delete","btn_value":"Delete","status":"D","active":true},
+                        {"name":"approve","btn_value":"Approve","status":"Y","active":true},                                        
+                        {"name":"approval_request","btn_value":"Approval request","status":"P","active":true},
+                        {"name":"close","btn_value":"Close","active":true},
+                        {"name":"add","btn_value":"Add New","active":true}
+                    ];
+                    break;
+                    case "Y":
+                    buttons=[                                       
+                        {"name":"update","btn_value":"Update","status":"U","active":true},
+                        {"name":"delete","btn_value":"Delete","status":"D","active":true},                  
+                        {"name":"disapprove","btn_value":"DisApprove","status":"N","active":true},                    
+                        {"name":"close","btn_value":"Close","active":true},
+                        {"name":"add","btn_value":"Add New","active":true}
+                    ];
+                    break;}
+                }
                 return buttons;
             }
             return factory;
