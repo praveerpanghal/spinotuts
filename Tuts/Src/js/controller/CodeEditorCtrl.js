801bd2851bfd458eafeee7e38d59f6809da8ec5f
balakrishnavasa <balakrishna@spinoinc.com>
2017-10-13 20:01:11 +0530

    adding code editor to project

---
+++ Tuts/Src/js/controller/CodeEditorCtrl.js
@@ -0,0 +1,28 @@
+app.controller('CodeEditorCtrl', ['$route','JsonDataService','httpCall',
+function ($route,JsonDataService,httpCall) {
+    var vm=this;
+    var jsonurl=JsonDataService.GetJsonInfo($route.current.templateUrl);
+    httpCall.GetMethod(jsonurl)               
+        .then(function(result) {   
+        vm.JsonVals = result;  
+     }, 
+     function(error) { 
+        console.log(error.statusText);
+      $log.info(error);
+    });
+
+    vm.data="<h2>welcome to live editer</h2>";
+    // new Jotted(document.querySelector('#jotted-demo-bin'), 
+    new Jotted(document.querySelector('#jotted-demo-codemirror'), 
+    {
+     files: [
+        {type: 'html',content: vm.data},
+      {type: 'css',content: ''},
+        {type: 'js',content: ''}
+      ],
+      pane: 'html',    
+        plugins:[
+        'codemirror'
+      ]
+    });
+}]);
